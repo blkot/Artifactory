c4b304623c53f37250ee4d6fc1ddd40553cbb9f5
Tom Wang <wangshenhao@hotmail.com>
2026-05-17 22:02:26 +0800

    fix disable New Architecture, fix route name to kits/[id]/index

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,11 +2,11 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="(auth)/login" options={{ presentation: "modal" }} />
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
       <Stack.Screen name="kits/new" options={{ headerShown: true, title: "Add Kit" }} />
-      <Stack.Screen name="kits/[id]" options={{ headerShown: true, title: "Kit Detail" }} />
+      <Stack.Screen name="kits/[id]/index" options={{ headerShown: true, title: "Kit Detail" }} />
       <Stack.Screen name="settings/filters" options={{ headerShown: true, title: "Filters" }} />
     </Stack>
   );
