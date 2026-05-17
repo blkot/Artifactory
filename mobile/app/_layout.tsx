baf8c465097c74310a2beddf0186e518b741fdf0
Tom Wang <wangshenhao@hotmail.com>
2026-05-17 22:59:47 +0800

    fix use Slot instead of Stack to bypass New Architecture boolean type bug

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,13 +1,5 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
-      <Stack.Screen name="kits/new" options={{ headerShown: true, title: "Add Kit" }} />
-      <Stack.Screen name="kits/[id]/index" options={{ headerShown: true, title: "Kit Detail" }} />
-      <Stack.Screen name="settings/filters" options={{ headerShown: true, title: "Filters" }} />
-    </Stack>
-  );
+  return <Slot />;
 }
